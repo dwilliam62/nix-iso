8c761644c445a1cec42cb4cedc4f9aa2c4920ae9
Don Williams <don.e.williams@gmail.com>
2025-12-27 14:25:31 -0500

    adding hideusers to configuration.nix
     On branch ddubsos-iso
     Your branch is up to date with 'origin/ddubsos-iso'.
    
     Changes to be committed:
    	modified:   scripts/install-hyprland-btw.sh

--- scripts/install-hyprland-btw.sh
+++ scripts/install-hyprland-btw.sh
@@ -287,12 +287,11 @@ awk '
   { print }
 ' ./configuration.nix > ./configuration.nix.tmp && mv ./configuration.nix.tmp ./configuration.nix
 
-# Also update ly settings with expanded hideUsers list
+# Add hideUsers to ly settings block to hide nix build users
 awk '
-  /^[[:space:]]*pbigclock = true;/ {
+  /^[[:space:]]*clock_color = / {
     print
-    print "        # Hide system and build users (note: ly may not fully respect this)"
-    print "        hideUsers = \"root,nobody,_flatpak,systemd-timesync,systemd-network,systemd-resolve,systemd-coredump,ntp,nixbld,nixbld1,nixbld2,nixbld3,nixbld4,nixbld5,nixbld6,nixbld7,nixbld8,nixbld9,nixbld10,nixbld11,nixbld12,nixbld13,nixbld14,nixbld15,nixbld16,nixbld17,nixbld18,nixbld19,nixbld20,nixbld21,nixbld22,nixbld23,nixbld24,nixbld25,nixbld26,nixbld27,nixbld28,nixbld29,nixbld30,nixbld31,nixbld32\";"
+    print "        hideUsers = \"root,nobody,_flatpak,systemd-timesync,systemd-network,systemd-resolve,systemd-coredump,ntp,nixbld,nixbld1,nixbld2,nixbld3,nixbld4,nixbld5,nixbld6,nixbld7,nixbld8,nixbld9,nixbld10\";"
     next
   }
   { print }
